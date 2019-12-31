fd2299e2fccccfac3a73a235397b7eb72d5a3aee
kkojha47 <kkojha47@gmail.com>
2019-12-30 18:07:40 -0700

    npm start working

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {register} from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import { BrowserRouter } from 'react-router-dom';
@@ -12,4 +12,4 @@ ReactDOM.render(
         <App />
     </BrowserRouter>
     , document.getElementById('root'));
-registerServiceWorker();
+register();
