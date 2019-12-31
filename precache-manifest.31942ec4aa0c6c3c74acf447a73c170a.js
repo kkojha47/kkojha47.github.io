2b78dd281730a6c4701c7b04b16e14eb1916987c
kkojha47 <kkojha47@gmail.com>
2019-12-30 18:48:07 -0700

    Updates

---
+++ precache-manifest.31942ec4aa0c6c3c74acf447a73c170a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cdf32148c2c887d2c979520a824f95f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ec73ac0b04e28c0b76b5",
+    "url": "/portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "ff9e12e8ca838ada095b",
+    "url": "/portfolio/static/css/main.ac7809c1.chunk.css"
+  },
+  {
+    "revision": "ec73ac0b04e28c0b76b5",
+    "url": "/portfolio/static/js/2.2eb08a32.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/portfolio/static/js/2.2eb08a32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff9e12e8ca838ada095b",
+    "url": "/portfolio/static/js/main.f8b0b16d.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  }
+]);
